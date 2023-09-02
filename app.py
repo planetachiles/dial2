2b0170f53ad4e4be18091402197240e5dd2d5056
planetachiles <67108795+planetachiles@users.noreply.github.com>
2023-09-02 17:31:10 +0000

    dial2 app privacy

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='template')
 
 @app.route("/")
  
 def hello_world() :
-   return "hello Shehu"
+  #return "<h1>hello, world!</h1>"
+  return render_template("home.html")
   
     
- #if __name__ == "__main__":
+#if __name__ == "__main__" :
+
 app.run(host='0.0.0.0', debug=True)
